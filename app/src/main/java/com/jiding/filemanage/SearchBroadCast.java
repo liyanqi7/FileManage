17e53903973989a3d51b4c19acd3283211bc9be9
Jerry <2113004123@qq.com>
2017-12-06 15:41:39 +0800

    修改后的SearchBroadcast

--- app/src/main/java/com/jiding/filemanage/SearchBroadCast.java
+++ app/src/main/java/com/jiding/filemanage/SearchBroadCast.java
@@ -3,8 +3,10 @@ package com.jiding.filemanage;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class SearchBroadCast extends BroadcastReceiver {
+    String TAG = "";
     public static String mServiceKeyword = "";//接收搜索关键字的静态变量
     public static String mServiceSearchPath = "";//接收搜索路径的静态变量
 
@@ -14,6 +16,7 @@ public class SearchBroadCast extends BroadcastReceiver {
         if (MainActivity.KEYWORD_BROADCAST.equals(mAction)){
             mServiceKeyword = intent.getStringExtra("keyword");
             mServiceSearchPath = intent.getStringExtra("searchPath");
+            Log.d(TAG, "onReceive: ");
         }
     }
 }
